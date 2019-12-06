6c91c35ade04009f9ac6088918672227ebff1b27
si-tayeb sofiane <sitayebsofiane51@gmail.com>
2019-12-06 13:51:10 +0100

    typo

---
+++ part.py
@@ -0,0 +1,23 @@
+from player import Player
+from game import Game
+
+class Part:
+
+    def __init__(self,player):
+        # instanciation of game
+        self.player=player
+        self.player.choix_deficulte()
+        self.game=Game(self.player)
+
+    
+    def part(self):
+        
+        while self.game.serie_simon():pass
+        self.game.player.score=len(self.game.serie)-1
+        return " a un  score de {} avec le niveau {} .".format(self.game.player.score,self.game.player.deficulte)
+
+
+        
+
+
+
